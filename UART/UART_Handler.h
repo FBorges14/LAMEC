43ba2317fb666b3e4bc0c255adf940c4c06b82ce
Fábio Borges <fabioborges14@icloud.com>
2015-11-10 00:51:47 +0000

    ->criação de pasta organizada para XBee

--- UART/UART_Handler.h
+++ UART/UART_Handler.h
@@ -5,6 +5,8 @@
 //verificar e obter byte
 uint8_t Lib_GetUARTInBufByte(uint8_t *inbyte);
 
+void Lib_SetUARTOutBufBytes(unsigned char *msg, char numbytes);
+
 //transmitir informaçao por IT
 HAL_StatusTypeDef Lib_UART_Transmit_IT(UART_HandleTypeDef *huart, uint8_t * data, uint16_t Size);
 
